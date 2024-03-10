44e9fb95944e77481a75da55ca7bae420190e03f
Tanuj Gupta <tanujg722@gmail.com>
2024-03-10 09:35:03 +0530

    add new question

--- third_file.py
+++ third_file.py
@@ -5,3 +5,9 @@ import datetime
 now = datetime.datetime.now()
 print("Current date and time is: ")
 print(now,strftime("%y-%m-%d %H:%M:%S"))
+
+
+# How to find area of a circle in python.
+from math import pi
+r = float(input("write the radius of the ciorcle: "))
+print("the arae of the circle is: " str(r) + "is: " + str(pi * r**2))
